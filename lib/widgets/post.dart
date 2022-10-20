cf98a0462c74a53e81bd74c4b3d39499f9682c7c
syusyu <syusyu0503@douzone.com>
2022-10-20 18:10:39 +0900

    16 custom progress indicator 만들기

---
+++ lib/widgets/post.dart
@@ -0,0 +1,35 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:dinosgram/widgets/my_progress_indicator.dart';
+import 'package:flutter/material.dart';
+
+class Post extends StatelessWidget {
+  final int index;
+  Size? size;
+
+  Post(
+    this.index, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    size ??= MediaQuery.of(context).size;
+
+    return CachedNetworkImage(
+      imageUrl: 'https://picsum.photos/id/$index/200/200',
+      placeholder: (BuildContext context, String url) {
+        return MyProgressIndicator(progressSize: 50);
+      },
+      imageBuilder: (BuildContext context, ImageProvider imageProvider) {
+        return AspectRatio(
+          aspectRatio: 1,
+          child: Container(
+            decoration: BoxDecoration(
+                image:
+                    DecorationImage(image: imageProvider, fit: BoxFit.cover)),
+          ),
+        );
+      },
+    );
+  }
+}
